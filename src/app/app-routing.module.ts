81fd53001b631926eb205e30a1d750a55086315d
Bohdankm22 <bohdansharipov@gmail.com>
2018-01-31 18:42:59 -0500

    Added heroes route in routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  exports: [RouterModule],
+  imports: [ RouterModule.forRoot(routes) ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
